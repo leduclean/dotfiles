390c016b5c9a8aa21bfc2a6ad9b1676d52a76f88
Le Duc Léandre <leandre.leduc38@gmail.com>
2025-06-29 19:58:04 +0200

    fix(i3): New settings to handle monitors layout changes

--- .config/i3/scripts/setup_monitors.sh
+++ .config/i3/scripts/setup_monitors.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo 3
+# Samsung old screen via usb link
+# xrandr   --output eDP --primary --mode 3072x1920 --output DVI-I-1-1 --mode 1920x1080 --above eDP  --auto
 
-sleep 3
-
-# Configurer l’écran externe
-xrandr   --output eDP --primary --mode 3072x1920   --output DVI-I-1-1 --mode 1920x1080 --above eDP   --auto --scale 1x1
+# AOC kalray screen
+xrandr   --output eDP --primary --mode 3072x1920 --output DisplayPort-1 --mode 1920x1200 --above eDP  --auto
